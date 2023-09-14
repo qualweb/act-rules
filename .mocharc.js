29d54f34eac5bd3ff930160cc9ac9a3e430e12b5
Johannes L. Borresen <jlb@inqludeit.dk>
2023-09-14 13:35:26 +0200

    test: refactor rule.spec.mjs
    Now runs all rules, and code has been refactored to properly setup/teardown.

---
+++ .mocharc.js
@@ -0,0 +1,13 @@
+// process.env.TS_NODE_PROJECT = "tsconfig.test.json"
+
+module.exports = {
+  "color": true,
+  "ui": "bdd",
+  "file": [
+  ],
+  "spec": "test/**/*.spec.mjs",
+  // "require": [
+  //   "tsconfig-paths/register",
+  //   "ts-node/esm",
+  // ],
+}
