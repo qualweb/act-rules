50b52534c8530fb288d5acce00a35e3dbcc9ac5e
lindhart <johannes@the.homestead.dk>
2023-09-18 23:35:51 +0200

    test: lint

--- .mocharc.js
+++ .mocharc.js
@@ -1,11 +1,5 @@
-// process.env.TS_NODE_PROJECT = "tsconfig.test.json"
-
 module.exports = {
   "color": true,
   "ui": "bdd",
   "spec": "test/dumb.spec.mjs",
-  // "require": [
-  //   "tsconfig-paths/register",
-  //   "ts-node/esm",
-  // ],
 }
